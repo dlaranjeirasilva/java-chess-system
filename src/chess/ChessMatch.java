413eb73f3cf44725c97767142d8ebe3e15f9ba48
dlaranjeirasilva <doug.ark@gmail.com>
2021-11-16 23:25:08 -0300

    Fix - better treatment to promotion

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,5 @@
 package chess;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -175,7 +174,7 @@ public class ChessMatch {
 		}
 		
 		if(!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
-			throw new InvalidParameterException("Invalid type for promotion");
+			return promoted;
 		}
 		
 		//Remove the instanceof Pawn found in the performChessMove test
